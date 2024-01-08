5badd7fa8de635d6e13a4222c4a70064ca850a62
Reza-Riswandha <emrezariswanda@gmail.com>
2024-01-08 16:55:03 +0700

    Add interface HewanDao

--- app/src/main/java/com/example/finalproject_pam/data/HewanDao.kt
+++ app/src/main/java/com/example/finalproject_pam/data/HewanDao.kt
@@ -1,4 +1,28 @@
 package com.example.finalproject_pam.data
 
-class HewanDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface HewanDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(hewan: Hewan)
+
+    @Update
+    suspend fun update(hewan: Hewan)
+
+    @Delete
+    suspend fun delete(hewan: Hewan)
+
+    @Query("SELECT * from tblHewan WHERE JenisHewan = :JenisHewan")
+    fun getHewan(JenisHewan: String): Flow<Hewan>
+
+    @Query("SELECT * from tblHewan ORDER BY JenisHewan ASC")
+    fun getAllHewan(): Flow<List<Hewan>>
 }
