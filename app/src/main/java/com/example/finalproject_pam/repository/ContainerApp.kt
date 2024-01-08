21f700f5b6f95751d884f4599a5db33f72c54ed6
Reza-Riswandha <emrezariswanda@gmail.com>
2024-01-09 00:31:44 +0700

    Add ContainerApp

--- app/src/main/java/com/example/finalproject_pam/repository/ContainerApp.kt
+++ app/src/main/java/com/example/finalproject_pam/repository/ContainerApp.kt
@@ -1,2 +1,15 @@
 package com.example.finalproject_pam.repository
 
+import android.content.Context
+import com.example.finalproject_pam.data.DatabaseHewan
+
+
+interface ContainerApp {
+    val repositoriHewan : RepositoriHewan
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriHewan: RepositoriHewan by lazy {
+        OfflineRepositoriHewan(DatabaseHewan.getDatabase(context).hewanDao())
+    }
+}
