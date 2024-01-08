f6439261f985d9e6d5bf7f999284bfa0f3df97e6
ACER <ACER@DESKTOP-QH5VR0V>
2024-01-09 00:15:18 +0700

    Add RepositoriHewan

---
+++ app/src/main/java/com/example/finalproject_pam/repository/RepositoriHewan.kt
@@ -0,0 +1,17 @@
+package com.example.finalproject_pam.repository
+
+import com.example.finalproject_pam.data.Hewan
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriHewan {
+
+    fun getAllHewanStream(): Flow<List<Hewan>>
+
+    fun getHewanStream(id: Int): Flow<Hewan>
+
+    suspend fun insertHewan(hewan: Hewan)
+
+    suspend fun deleteHewan(hewan: Hewan)
+
+    suspend fun updateHewan(hewan: Hewan)
+}
