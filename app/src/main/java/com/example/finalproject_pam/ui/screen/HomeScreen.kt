a461a338b9ff192c062c2fb3bed07e3b2dffcdf7
ACER <ACER@DESKTOP-QH5VR0V>
2024-01-09 02:12:54 +0700

    Add DataHewan and Listhewan in HomeScreen

--- app/src/main/java/com/example/finalproject_pam/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/screen/HomeScreen.kt
@@ -1,2 +1,116 @@
 package com.example.finalproject_pam.ui.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.ShoppingCart
+
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+import com.example.finalproject_pam.R
+import com.example.finalproject_pam.data.Hewan
+
+@Composable
+fun ListHewan(
+    itemHewan : List<Hewan>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Hewan) -> Unit
+){
+    LazyColumn(modifier = modifier){
+        items(items = itemHewan, key = {it.id}){
+                person ->
+            DataHewan(
+                hewan = person,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)).clickable{onItemClick(person)})
+        }
+    }
+}
+
+@Composable
+fun DataHewan(
+    hewan: Hewan,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.pets),
+                    contentDescription = "jenis",
+                    modifier = Modifier.size(10.dp).weight(0.2f)
+                )
+                Text(
+                    text = hewan.JenisHewan,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.gender),
+                    contentDescription = "gender",
+                    modifier = Modifier.size(10.dp).weight(0.2f)
+                )
+                Text(
+                    text = hewan.JenisKelamin,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = "usia",
+                    modifier = Modifier.size(10.dp).weight(0.2f)
+                )
+                Text(
+                    text = hewan.Usia,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = "Harga",
+                    modifier = Modifier.size(10.dp).weight(0.2f)
+                )
+                Text(
+                    text = hewan.Harga,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            
+        }
+    }
+}
