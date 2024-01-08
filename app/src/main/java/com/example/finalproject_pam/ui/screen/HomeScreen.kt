b60de90043b7a60210f74ba62ec20933f6eddf9b
Reza-Riswandha <emrezariswanda@gmail.com>
2024-01-09 02:23:38 +0700

    Add bodyscreen in HomeScreen.kt

--- app/src/main/java/com/example/finalproject_pam/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/screen/HomeScreen.kt
@@ -21,13 +21,42 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 import com.example.finalproject_pam.R
 import com.example.finalproject_pam.data.Hewan
+
+@Composable
+fun BodyScreen(
+    itemHewan: List<Hewan>,
+    modifier: Modifier=Modifier,
+    onHewanClick: (Int) -> Unit = {}
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemHewan.isEmpty()){
+            Text(
+                text = stringResource(id = R.string.required),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }else{
+            ListHewan(
+                itemHewan = itemHewan,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small)),
+                onItemClick = {onHewanClick(it.id)}
+            )
+        }
+    }
+}
 
 @Composable
 fun ListHewan(
