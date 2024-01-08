2d6354d6682b41a78d8de2ee6563e1090c724a24
Reza-Riswandha <emrezariswanda@gmail.com>
2024-01-09 01:09:30 +0700

    Add HomeViewModel

--- app/src/main/java/com/example/finalproject_pam/ui/model/HomeViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/model/HomeViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.finalproject_pam.ui.model
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject_pam.data.Hewan
+import com.example.finalproject_pam.repository.RepositoriHewan
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel (private val repositoriHewan: RepositoriHewan): ViewModel(){
+
+    companion object{
+        private  const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriHewan.getAllHewanStream()
+        .filterNotNull()
+        .map { HomeUiState(listHewan = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+    data class HomeUiState(
+        val listHewan: List<Hewan> = listOf()
+    )
+}
